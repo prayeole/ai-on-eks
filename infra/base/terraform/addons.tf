e3a690d36f428f2aa14ce134ce4438a21947599c
omrishiv <327609+omrishiv@users.noreply.github.com>
2025-10-20 11:11:13 -0700

    include empty addons.tf for upgrades
    Signed-off-by: omrishiv <327609+omrishiv@users.noreply.github.com>

---
+++ infra/base/terraform/addons.tf
@@ -0,0 +1,2 @@
+# Leaving an empty addons.tf file temporarily to ensure old clusters are upgraded properly by forcing this file to be empty for upgrades.
+# This file will be removed at a later date.
