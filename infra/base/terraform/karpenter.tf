0d6a1ee738d924e0c2fd34868cd442a07653d728
Erez Zarum <erezz@amazon.com>
2025-10-22 15:54:21 +0300

    Move Helm creds to provider

--- infra/base/terraform/karpenter.tf
+++ infra/base/terraform/karpenter.tf
@@ -28,9 +28,6 @@ resource "helm_release" "karpenter" {
   version          = var.karpenter_version
   wait             = true
 
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
-
   values = [
     <<-EOT
     nodeSelector:
